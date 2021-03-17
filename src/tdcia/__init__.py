ec5d77faadff575fff23a9a9bf483b9907b2d4a3
iulusoy <inga.ulusoy@uni-heidelberg.de>
2021-03-17 12:18:30 +0100

    fixed flake8 import issues

--- src/tdcia/__init__.py
+++ src/tdcia/__init__.py
@@ -1,4 +1,9 @@
 # __init__.py
-from .input_output import *
-from .numerical import *
-from .statistical import *
+from .input_output import input_data  # noqa
+from .input_output import output_data  # noqa
+from .numerical import aucofu  # noqa
+from .numerical import calc_auto  # noqa
+from .numerical import DFT  # noqa
+from .statistical import check_significance  # noqa
+from .statistical import euclidean_distance  # noqa
+from .statistical import correlation_matrix  # noqa
